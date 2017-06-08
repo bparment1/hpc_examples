2ed8035f5a478273b5d7a74bf91849070a3c23b7
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2017-06-08 14:30:00 -0400

    adding log error to slurm job

--- exercise_slurm_test_fire_alaska_analyses.sh
+++ exercise_slurm_test_fire_alaska_analyses.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=benoit_workshop_ex2_test    # Job name
-#SBATCH --mail-type=ALL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=bparmentier@sesync.org   # Where to send mail	
-#SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --time=00:25:00               # Time limit hrs:min:sec
-#SBATCH --output=serial_test_%j.out   # Standard output and error log
+#SBATCH --job-name=bp_workshop_ex2_array_test    # Job name
+#SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
+#SBATCH	--array=1-2                              # job array, two jobs
+#SBATCH --ntasks=1                               # each array task gets a single CPU core
+#SBATCH --time=00:50:00                          # Time limit hrs:min:sec
+#SBATCH --output=bp_workshop_ex2_array_test_%A_%a.out   # Standard output from console
+#SBATCH --error=bp_workshop_ex2_array_test_%A_%a.err   # Error log 
 #SBATCH --partition=sesynctest        # queue name, this is for debugging and testing
 
 pwd; hostname; date
  
 echo "Running exercise 2 workshop on a single CPU core"
  
-Rscript --vanilla exercise2_fire_alaska_analyses_05112017.R
+Rscript --vanilla /nfs/bparmentier-data/Data/SLURM_test/scripts/exercise_slurm_test_fire_alaska_analyses_06092017.R
