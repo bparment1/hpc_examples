a27a84d96cd84b43f2cb82f4a88a2f4817731d39
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2017-06-09 14:30:00 -0400

    slight changes and test of slurm array job script

--- exercise_slurm_test_fire_alaska_analyses.sh
+++ exercise_slurm_test_fire_alaska_analyses.sh
@@ -2,11 +2,9 @@
 #SBATCH --job-name=bp_workshop_ex2_array_test    # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
-#SBATCH	--array=1-2                              # job array, two jobs
-#SBATCH --ntasks=1                               # each array task gets a single CPU core
 #SBATCH --time=00:50:00                          # Time limit hrs:min:sec
-#SBATCH --output=bp_workshop_ex2_array_test_%A_%a.out   # Standard output from console
-#SBATCH --error=bp_workshop_ex2_array_test_%A_%a.err   # Error log 
+#SBATCH --output=bp_workshop_ex2_array_test_%j.out   # Standard output from console
+#SBATCH --error=bp_workshop_ex2_array_test_%j.err   # Error log 
 #SBATCH --partition=sesynctest        # queue name, this is for debugging and testing
 
 pwd; hostname; date
