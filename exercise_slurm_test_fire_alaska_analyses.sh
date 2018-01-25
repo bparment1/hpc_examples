6b114df5ddb2d79ff1556d1004989c6426b13b81
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-01-25 14:30:00 -0500

    example script for array slurm job with geographic data

--- exercise_slurm_test_fire_alaska_analyses.sh
+++ exercise_slurm_test_fire_alaska_analyses.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=bp_workshop_ex2_array_test    # Job name
+#SBATCH --job-name=slurm_test_spatial_array_test    # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
 #SBATCH	--array=1-4                              # job array, four jobs corresponding to four tiles in Alaska
 #SBATCH --ntasks=1                               # each array task gets a single CPU core
 #SBATCH --time=00:50:00                          # Time limit hrs:min:sec
-#SBATCH --output=bp_workshop_ex2_array_test_%A_%a.out   # Standard output from console
-#SBATCH --error=bp_workshop_ex2_array_test_%A_%a.err   # Error log 
+#SBATCH --output=console_%A_%a.out   # Standard output from console
+#SBATCH --error=log_%A_%a.err   # Error log 
 #SBATCH --partition=sesynctest        # queue name, this is for debugging and testing
 
 pwd; hostname; date
  
 echo "Running exercise 2 workshop on a single CPU core"
  
-Rscript --vanilla /nfs/bparmentier-data/Data/SLURM_test/scripts/exercise_slurm_test_fire_alaska_analyses_06092017b.R
+Rscript --vanilla /nfs/bparmentier-data/Data/SLURM_test/scripts/exercise_slurm_test_fire_alaska_analyses_01252018.R
